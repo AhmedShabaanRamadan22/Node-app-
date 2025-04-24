5c3fe34a41c313a3101602d658109bc5f9f41d67
Ahmed shabaan <135539885+AhmedShabaanRamadan22@users.noreply.github.com>
2025-04-24 13:59:49 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Dockerized Node.js App! by Ahmed shabaan');
+  res.send('Hello from Dockerized Node.js App! by Ahmed shaban');
 });
 
 app.listen(port, () => {
